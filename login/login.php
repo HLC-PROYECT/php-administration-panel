d71677f49ba1db873fabca6d22d26177aa3571e9
Victor Daniel Gil Becerra <victoryil27oficial@gmail.com>
2021-02-11 00:32:05 +0100

    comprobacion y base de datos

--- login/login.php
+++ login/login.php
@@ -1,2 +1,25 @@
 <?php
-echo "Pollo";
+require '../utils/Medoo.php';
+
+use Medoo\Medoo;
+
+$database = new Medoo([
+    'database_type' => 'mysql',
+    'database_name' => 'task_manager',
+    'server' => 'localhost',
+    'username' => 'root',
+    'password' => ''
+]);
+if (!empty(trim($_POST["email"])) && !empty(trim($_POST["password"]))) {
+    $response = $database->select("usuario", "*", ["email" => $_POST["email"], "password" => $_POST["password"]]);
+    if ($response != null) {
+        $r = $response[0];
+        header("Location: ../testClasses/logComplete.php?response=".$r["id"]);
+    } else {
+        $error = true;
+    }
+
+} else {
+    $error = true;
+}
+?>
--- testClasses/logComplete.php
+++ testClasses/logComplete.php
@@ -1,3 +1,17 @@
+<?php
+require '../utils/Medoo.php';
+
+use Medoo\Medoo;
+
+$database = new Medoo([
+    'database_type' => 'mysql',
+    'database_name' => 'task_manager',
+    'server' => 'localhost',
+    'username' => 'root',
+    'password' => ''
+]);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -10,7 +24,7 @@
     <meta name="keywords" content="au theme template">
 
     <!-- Title Page-->
-    <title>Login</title>
+    <title>Badge</title>
 
     <!-- Fontfaces CSS-->
     <link href="../template/css/font-face.css" rel="stylesheet" media="all">
@@ -35,71 +49,44 @@
     <link href="../template/css/theme.css" rel="stylesheet" media="all">
 
 </head>
+
 <body class="animsition">
-<form action="login.php" method="post">
 
-    <div class="page-wrapper">
-        <div class="page-content--bge5">
-            <div class="container">
-                <div class="login-wrap">
-                    <div class="login-content">
-                        <div class="login-logo">
-                            <a href="#">
-                                <img src="../template/images/icon/logo.png" alt="CoolAdmin">
-                            </a>
-                        </div>
-                        <div class="login-form">
-                            <form action="" method="post">
-                                <div class="form-group">
-                                    <label>Email Address</label>
-                                    <input required class="au-input au-input--full" type="email" name="email"
-                                           placeholder="Email">
-                                </div>
-                                <div class="form-group">
-                                    <label>Password</label>
-                                    <input required class="au-input au-input--full" type="password" name="password"
-                                           placeholder="Password">
-                                </div>
-                                <div class="login-checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember">Remember Me
-                                    </label>
-                                </div>
-                                <button class="au-btn au-btn--block au-btn--green m-b-20" type="submit">sign in</button>
-                            </form>
-                            <div class="register-link">
-                                <p>
-                                    Don't you have account?
-                                    <a href="#">Sign Up Here</a>
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
+<div class="card">
+    <div class="card-header">
+        <strong>Usuario</strong>
     </div>
-</form>
+    <div class="card-body">
+
+        <?php
+
+        $res = $database -> select("usuario","*",["id"=>$_GET["response"]]);
+        echo 'Correo: ';
+        echo '<span class="badge badge-success">' . $res[0]["email"] . '</span>';
+        echo '<br/>';
+        echo 'Contraseña: ';
+        echo '<span class="badge badge-info">' . $res[0]["password"] . '</span>';
+        ?>
+    </div>
+</div>
+
+</div>
 
 <!-- Jquery JS-->
 <script src="../template/vendor/jquery-3.2.1.min.js"></script>
 <!-- Bootstrap JS-->
 <script src="../template/vendor/bootstrap-4.1/popper.min.js"></script>
 <script src="../template/vendor/bootstrap-4.1/bootstrap.min.js"></script>
-<!-- Vendor Jtemplate/S       -->
+<!-- Vendor JS       -->
 <script src="../template/vendor/slick/slick.min.js">
 </script>
-template/
 <script src="../template/vendor/wow/wow.min.js"></script>
 <script src="../template/vendor/animsition/animsition.min.js"></script>
 <script src="../template/vendor/bootstrap-progressbar/bootstrap-progressbar.min.js">
 </script>
-template/
 <script src="../template/vendor/counter-up/jquery.waypoints.min.js"></script>
 <script src="../template/vendor/counter-up/jquery.counterup.min.js">
 </script>
-template/
 <script src="../template/vendor/circle-progress/circle-progress.min.js"></script>
 <script src="../template/vendor/perfect-scrollbar/perfect-scrollbar.js"></script>
 <script src="../template/vendor/chartjs/Chart.bundle.min.js"></script>
@@ -113,4 +100,4 @@ template/
 
 </html>
 
-
+<!-- end document-->
