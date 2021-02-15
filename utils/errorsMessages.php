ad8212f0db9abebad9d46c98fcc41972271213ae
Juan Antonio Tejada <37996903+juan31tp@users.noreply.github.com>
2021-02-15 10:17:48 +0100

    Some little errors fixed

--- utils/errorsMessages.php
+++ utils/errorsMessages.php
@@ -2,17 +2,16 @@
 
 namespace errorsMessages;
 
-
 class errorsMessages
 {
     public function getError(string $errorCode):string
     {
         return match ($errorCode) {
-            "login:invalid" => "El usario o contraseña no son validos",
-            "email:invalid" => "Person email is not valid. Please enter a valid email",
-            "email:repeat" => "el correo que intentó usar ya esta registrado, Inicie sesion",
-            "password:invalid" => "Password is not valid. Must have almost 8 character",
-            default => "Error no controlado",
+            "login:invalid" => "The user or the password were not correct, please try again",
+            "email:invalid" => "Person's email is not valid. Please enter a valid email.",
+            "email:repeat" => "This mail is already in use. If its yours, try to login.",
+            "password:invalid" => "Password is not valid. Must have almost 8 characters.",
+            default => "Error not controlled.",
         };
     }
 }
