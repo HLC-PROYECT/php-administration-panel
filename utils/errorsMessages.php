2c482dc5a4560eaaa622ae834ac6c70e5e48c830
Juan Antonio Tejada <37996903+juan31tp@users.noreply.github.com>
2021-02-17 08:28:44 +0100

    Fix - Error messages translation

--- utils/errorsMessages.php
+++ utils/errorsMessages.php
@@ -8,12 +8,12 @@ class errorsMessages
     public function getError(string $errorCode):string
     {
         return match ($errorCode) {
-            "login:invalid" => "El usario o contraseña no son validos",
+            "login:invalid" => "The user or the password were not valid",
             "email:invalid" => "Person email is not valid. Please enter a valid email",
-            "email:repeat" => "el correo que intentó usar ya esta registrado, Inicie sesion",
-            "dni:repeat" => "el dni que intentó usar ya esta registrado, Inicie sesion",
-            "password:invalid" => "Password is not valid. Must have almost 8 character",
-            default => "Error no controlado",
+            "email:repeat" => "The mail you tried to use is already in use. Please, use another one.",
+            "dni:repeat" => "This DNI is already in use, just sign in!",
+            "password:invalid" => "Password is not valid. Must have almost 8 characters",
+            default => "Error not controlled",
         };
     }
 }
